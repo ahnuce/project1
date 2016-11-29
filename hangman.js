508ac193fcc7ad58df06aca95d10c498d1a4177f
yourUsername <your_email@example.com>
2016-11-29 16:43:55 -0500

    after pseudocoding first steps at coding

---
+++ hangman.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+
+var hangman = document.getElementById('hangman');
+var head = document.getElementById('head');
+var torso = document.getElementById('torso');
+var legs = document.getElementById('legs');
+var deathRope= document.getElementById('deathRope');
+var inputWord = document.getElementById('inputWord');
+var inputLetter = document.getElementById('inputLetter');
+var timeScore = 0;
+var scoreText = document.getElementById('scoreText');
+let wordInput = $('wordInput').val();
+//this is to save the users input
+
+function something(wordInput){
+  wordLength = wordInput.length;
+  console.log(wordLength);
+}
+
+
+
+
+
+
+};
