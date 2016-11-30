134da7bf835aaec71b82158f3715e8c57b0a9069
yourUsername <your_email@example.com>
2016-11-30 16:53:02 -0500

    started over but was able to save user input and change to array now

--- hangman.js
+++ hangman.js
@@ -1,25 +1,44 @@
-window.onload = function(){
 
-var hangman = document.getElementById('hangman');
-var head = document.getElementById('head');
-var torso = document.getElementById('torso');
-var legs = document.getElementById('legs');
-var deathRope= document.getElementById('deathRope');
-var inputWord = document.getElementById('inputWord');
-var inputLetter = document.getElementById('inputLetter');
-var timeScore = 0;
-var scoreText = document.getElementById('scoreText');
-let wordInput = $('wordInput').val();
+//set up the variables
+
+alphabetArray = ['A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'];
+var button = $('button');
+var startGame = $('startGame');
+var createLetterSelection = $('createLetterSelection');
+var makeAlphabetDivs;
+var $wordInput = $(wordInput);
+var x;
+var p = $('#wordInputArr');
 //this is to save the users input
 
-function something(wordInput){
-  wordLength = wordInput.length;
-  console.log(wordLength);
+
+//click to start game after user inputs word
+button.click(function(){
+  if (startGame){
+console.log('it works');
+//set a variable to the wordInput
+userWord = $wordInput.val();
+makeAlphabetDivs();
+}
+});
+
+//function to take userWord and split into divs
+function makeAlphabetDivs(){
+ x = userWord;
+ //takes the user input and splits it into array value
+ x = x.split('');
+  console.log(x);
+  p.val(x).hide();
+//***make it hidden. do later
+
+
+//take x value for array and split it into divs
+
 }
 
 
-
-
-
-
-};
+//
+// function createLetterDivs(){
+// for(i=0; i<wordInput.length; i++) {
+//     showMsg('it works');
+//
