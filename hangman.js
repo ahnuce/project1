c71ee26181706ad6dc5bcdcab68561dc94e78aef
yourUsername <your_email@example.com>
2016-12-01 11:18:53 -0500

    input has been split and values are arrayed

--- hangman.js
+++ hangman.js
@@ -1,14 +1,18 @@
 
 //set up the variables
-
-alphabetArray = ['A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'];
+var newHTML = [];
+var alphabetArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var button = $('button');
 var startGame = $('startGame');
 var createLetterSelection = $('createLetterSelection');
-var makeAlphabetDivs;
+var makeAlphabetArr;
 var $wordInput = $(wordInput);
-var x;
+var wordArray = [];
 var p = $('#wordInputArr');
+var divs = document.getElementsByClassName('divboard');
+var form = document.getElementById('wordInput');
+
+
 //this is to save the users input
 
 
@@ -18,23 +22,51 @@ button.click(function(){
 console.log('it works');
 //set a variable to the wordInput
 userWord = $wordInput.val();
-makeAlphabetDivs();
+makeAlphabetArr();
+spanValues();
+form.replaceWith('');
 }
 });
 
-//function to take userWord and split into divs
-function makeAlphabetDivs(){
- x = userWord;
+//function to take userWord and split into array
+function makeAlphabetArr(){
  //takes the user input and splits it into array value
- x = x.split('');
-  console.log(x);
-  p.val(x).hide();
+ wordArray = userWord.split('');
+  console.log(wordArray);
+  p.val(wordArray).hide();
+}
 //***make it hidden. do later
+
+
+//take array values and create spans for each value
+function spanValues(){
+  for (i=0; i < wordArray.length; i++){
+    $('body').append($(`<span> ${wordArray[i]} </span>`));
+    button.hide();
+  }
+}
+
+//replace wordArray values with underlines
+
+${wordArray[i]}
+
+
+
+
+
+// var container = document.getElementById("wordInputArr");
+// for (var i = 0; i < x.length; i++) {
+//    container.innerHTML += '<div class="wordInputArr"></div>';
+// }
+
 
 
 //take x value for array and split it into divs
 
-}
+
+
+
+
 
 
 //
